b9fc1bf7d3af80efe04b5f7d1e54206133900a06
kent <s9n9201@yahoo.com.tw>
2022-03-20 21:24:38 +0800

    add actions

---
+++ src/store/auth.module.js
@@ -0,0 +1,61 @@
+import AuthService from "@/services/auth.service";
+
+const user=JSON.stringify(localStorage.getItem("user"));
+const initialStat=user
+    ? { status: { loggedIn:true }, user }
+    : { status: { loggedIn:false }, user: null }
+
+export const auth={
+    namespaced: true,
+    state: initialStat,
+    actions: {
+        login({ commit }, user) {
+            return AuthService.login(user).then(
+                user => {
+                    commit("loginSuccess", user);
+                    return Promise.resolve(user);
+                },
+                error => {
+                    commit("loginFailure");
+                    return Promise.reject(error);
+                }
+            );
+        },
+        logout({ commit }) {
+            AuthService.logout();
+            commit("logout");
+        },
+        register({ commit }, user) {
+            return AuthService.register(user).then(
+                response => {
+                    commit("registerSuccess");
+                    return Promise.resolve(response.data);
+                },
+                error => {
+                    commit("registerFailure");
+                    return Promise.resolve(error);
+                }
+            );
+        }
+    },
+    mutations: {
+        loginSuccess(state, user) {
+            state.status.loggedIn=true;
+            state.user=user;
+        },
+        loginFailure(state) {
+            state.status.loggedIn=false;
+            state.status=null;
+        },
+        logout(state) {
+            state.status.loggedIn=false;
+            state.status=null;
+        },
+        registerSuccess(state) {
+            state.status.loggedIn=false;
+        },
+        registerFailure(state) {
+            state.status.loggedIn=false;
+        }
+    }
+}
