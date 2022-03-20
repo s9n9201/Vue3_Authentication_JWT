0ab7d4b250e0a09712a8eb86b5f769d4fe095cd4
kent <s9n9201@yahoo.com.tw>
2022-03-20 13:43:58 +0800

    import css

--- src/main.js
+++ src/main.js
@@ -3,4 +3,9 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import "@/assets/css/bootstrap.css"
+import "bootstrap-icons/font/bootstrap-icons.css"
+import "@/assets/css/app.css"
+import "@/assets/css/pages/auth.css"
+
 createApp(App).use(store).use(router).mount('#app')
