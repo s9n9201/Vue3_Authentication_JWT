18894c50958f8dd97c86179a9436cc76448daf9e
kent <s9n9201@yahoo.com.tw>
2022-03-22 18:02:28 +0800

    add beforEach

--- src/router/index.js
+++ src/router/index.js
@@ -3,13 +3,23 @@ import { createRouter, createWebHistory } from 'vue-router'
 const routes = [
     {
         path: "/",
+        name: "Main",
+        component: ()=>import("../components/Profile"),
+    },
+    {
+        path: "/login",
         name: "Login",
-        component: ()=>import("../components/Login")
+        component: ()=>import("../components/Login"),
     },
     {
         path: "/register",
         name: "Register",
         component: ()=>import("../components/Register")
+    },
+    {
+        path: "/profile",
+        name: "Profile",
+        component: ()=>import("../components/Profile")
     },
 ]
 
@@ -17,5 +27,15 @@ const router = createRouter({
     history: createWebHistory(process.env.BASE_URL),
     routes
 })
+
+router.beforeEach((to, form , next)=>{
+    const publicPages=["/login","/register"]
+    const loggedIn=localStorage.getItem("user");
+    if (!publicPages.includes(to.path) && !loggedIn) {
+        next("/login");
+    } else {
+        next();
+    }
+});
 
 export default router
