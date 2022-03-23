a13fcc552aa688c7ddfd91a5669adaab0647c357
kent <s9n9201@yahoo.com.tw>
2022-03-23 20:11:26 +0800

    first commit

---
+++ src/components/Home.vue
@@ -0,0 +1,32 @@
+<template>
+    <header>
+        <h2 class="card-title">{{ content }} Profile</h2>
+    </header>
+</template>
+
+<script>
+import UserService from "@/services/user.service";
+
+export default {
+    name: "HomeComponent",
+    data() {
+        return {
+            content: "",
+        }
+    },
+    mounted() {
+        UserService.getPublicContent().then(
+            (response)=>{
+                this.content=response.data;
+            },
+            (error)=>{
+                this.content=(error.response && error.response.data && error.response.message) || error.message || error.toString();
+            }
+        );
+    },
+}
+</script>
+
+<style scoped>
+
+</style>
