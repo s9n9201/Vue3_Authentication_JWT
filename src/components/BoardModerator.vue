ad9b92f5698ab0181f14d105b8db0e4ec72fa215
kent <s9n9201@yahoo.com.tw>
2022-03-23 20:32:32 +0800

    first commit

---
+++ src/components/BoardModerator.vue
@@ -0,0 +1,31 @@
+<template>
+    <header>
+        <h2 class="card-title">{{ content }} Profile</h2>
+    </header>
+</template>
+
+<script>
+import UserService from "@/services/user.service";
+export default {
+    name: "BoardUser",
+    data() {
+        return {
+            content: "",
+        }
+    },
+    mounted() {
+        UserService.getModeratorBoard().then(
+            (response)=>{
+                this.content=response.data;
+            },
+            (error)=>{
+                this.content=(error.response && error.response.data && error.response.data.message) || error.message || error.toString();
+            }
+        );
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
