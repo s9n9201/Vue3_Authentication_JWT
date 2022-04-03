51795adfba872eb502f73cc981200be01c9db19b
kent <s9n9201@yahoo.com.tw>
2022-04-03 22:18:50 +0800

    add axios Instance

--- src/main.js
+++ src/main.js
@@ -2,10 +2,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import setupInstance from "@/services/setupInstance";
 
 import "@/assets/css/bootstrap.css"
 import "bootstrap-icons/font/bootstrap-icons.css"
 import "@/assets/css/app.css"
 import "@/assets/css/pages/auth.css"
 
+setupInstance(store);
 createApp(App).use(store).use(router).mount('#app')
